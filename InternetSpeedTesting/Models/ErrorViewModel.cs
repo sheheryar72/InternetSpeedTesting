e411da847ffabd4ff57ddf2b6ce1d25e983d28f5
sheheryar72 <86529804+sheheryar72@users.noreply.github.com>
2022-09-27 02:08:24 +0500

    first

---
+++ InternetSpeedTesting/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InternetSpeedTesting.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
